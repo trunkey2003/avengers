59fdb76a4d79619932ab2afb21db5b83a75795b9
Ho Quang Lam <84451024+trunkey2003@users.noreply.github.com>
2021-05-31 14:24:45 +0700

    Update JS
    - An update get today's date through a get-date button 
    - Clickable and closable 'More' menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
 function Submit() {
     alert("Your comment have succesfully sent, LamTin will contact you later! Thanks !");
 }
+
+
+
+
+document.getElementById("more-button").onclick = function(){
+    document.getElementById("sub-nav").style.display = "block"; 
+    document.getElementById("close-sub-nav").style.display = "inline-block";
+    console.log('sub-nav');
+}
+
+document.getElementById('date-button').onclick = function() {
+    document.getElementById('date').innerHTML = Date();
+};   
+
+document.getElementById("close-sub-nav").onclick=function(e){
+    e.stopPropagation();
+    document.getElementById("sub-nav").style.display = "none";
+    document.getElementById("close-sub-nav").style.display = "none";
+};
+
+document.getElementById('date').onclick = function() {
+    document.getElementById('date').innerHTML ='';
+}; 
+
+
